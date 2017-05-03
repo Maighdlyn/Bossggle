d335ec62b7f32e1780908bb0a22690094c619b5b
Johnathan Mitchell <johnathanmitchell@LGs-MacBook-Pro.local>
2017-05-02 20:49:47 -0700

    Day two work, start of javascript

--- bossggle.js
+++ bossggle.js
@@ -1,31 +1,18 @@
-$(document).ready(function () {
-	var idArray = ["letter1", "letter2", "letter3", "letter4", "letter5", "letter6"];
-//	function arrayFunction() {
-		var lettersArray = [];
-		var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	//"AAAAAAAAABBCCDDDDEEEEEEEEEEEEFFGGGHHIIIIIIIIIJKLLLLMMNNNNNNOOOOOOOOPPQRRRRRRSSSSTTTTTTUUUUVVWWXYYZ";
-		for (i = 0; i < 16; i++) {
-			var randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
-			lettersArray.push(randomLetter);
-//			document.getElementById('letter1').textContent = randomLetter;
-//			document.getElementsByClassName('letters').textContent = randomLetter;
-		};
-		console.log("lettersArray = ", lettersArray);
-	
-//	document.getElementById('1').textContent = lettersArray[1];
+function arrayFunction() {
+  var lettersArray = [];
+  var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-		// function randomVowel(){
-		// 	  var vowels = "AEIOU"
-		// 	for (i = 0; i < 2; i++) {
-		//   	var randomVowel = vowels[Math.floor(Math.random() * vowels.length)];
-		//   	randomString += randomVowel;
-		//   	updateLetters(randomString);
-		// 	};
-		//};
+  for (i = 0; i < 16; i++) {
+    console.log("hi");
 
-//		$("#submit").click(function () {
-//			arrayFunction();
-//		});
+    var randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+    // lettersArray.push(randomLetter);
+    document.getElementById(i).innerHTML = randomLetter;
 
-//	};
-});
+
+   console.log("random letter = ", randomLetter);
+  };
+};
+
+
+ console.log(arrayFunction())
