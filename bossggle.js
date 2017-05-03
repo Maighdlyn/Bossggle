229d7bb65ec81f6c84b34e3f49c7a1be73ad25fe
Johnathan Mitchell <johnathanmitchell@LGs-MacBook-Pro.local>
2017-05-03 12:12:37 -0700

    Revised code: Work after coach queue help

--- bossggle.js
+++ bossggle.js
@@ -1,31 +1,38 @@
+(function(){
 
 
-function arrayFunction() {
+
   var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var letterBoxes = document.querySelectorAll(".letter")
+  var onLetterClick = function (event) {
+    console.log(event.target.innerText);
+  }
 
-  for (i = 1; i < 17; i++) {
+  for (var i = 0; i < letterBoxes.length; i++) {
+    var randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+    letterBoxes[i].innerHTML = randomLetter;
+  }
+  for (var i = 0; i < letterBoxes.length; i++) {
+    letterBoxes[i].addEventListener('click', onLetterClick)
+  }
 
-     var randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
-     document.getElementById(i).innerHTML = randomLetter;
-
-   };
-};
 
 
-arrayFunction();
 
-var spelledWord = [];
+  var spelledWord = [];
 
-function selectLetter () {
-  document.getElementsByClassName("letter-default").className = "selected-letter";
-  spelledWord.push(this.textContent());
+  function selectLetter () {
+    document.querySelectorAll(".letter")
+    // .className = "selected-letter";
+    spelledWord.push(this.textContent());
 
-}
+  }
 
-console.log('spelledWord');
-// window.onload = function()
-//     {
-//   document.getElementsByClassName("letter-default").addEventListener( 'click', selectedLetter);
-//     };
+  console.log('spelledWord');
+  // window.onload = function()
+  //     {
+  //   document.getElementsByClassName("letter-default").addEventListener( 'click', selectedLetter);
+  //     };
 
-/**  **/
+  /**   **/
+})()
