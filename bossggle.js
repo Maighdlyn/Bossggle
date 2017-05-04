aa4d6f4a816adff55e82377cad69adc9d7417cc6
Emma <maighdlyn@Astra.local>
2017-05-03 19:28:55 -0700

    Fixed formatting issue and cleaned up code

--- bossggle.js
+++ bossggle.js
@@ -1,54 +1,41 @@
-(function(){
+(function () {
+	var chosenLetters = '';
+	var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	var letterBoxes = document.querySelectorAll(".letter")
 
+	var onLetterClick = function () {
+		event.target.classList.add('selected-letter')
+		chosenLetters += event.target.innerText
+		document.getElementById("word").innerText = chosenLetters;
+	}
 
-  var chosenLetters = '';
-  var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var letterBoxes = document.querySelectorAll(".letter")
-  var onLetterClick = function (event) {
+	for (var i = 0; i < letterBoxes.length; i++) {
+		var randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+		letterBoxes[i].innerHTML = randomLetter;
+		letterBoxes[i].addEventListener('click', onLetterClick)
+	}
 
-    event.target.classList.add('selected-letter')
-    chosenLetters += event.target.innerText
-    // spelledWord.push(event.target.innerText)
-    // var spelledStr = spelledWord.join("");
-    document.getElementById("word").innerText = chosenLetters;
+	document.querySelector('#clear').addEventListener('click', clear)
+	document.querySelector('#submit').addEventListener('click', submit)
+	document.querySelector('#reset').addEventListener('click', reset)
 
-  }
+	function clear() {
+		var selectedLetters = document.querySelectorAll(".selected-letter");
+		for (var i = 0; i < selectedLetters.length; i++) {
+			selectedLetters[i].classList.remove("selected-letter")
+		}
+		document.getElementById("word").innerText = chosenLetters = '';
+	}
 
-  for (var i = 0; i < letterBoxes.length; i++) {
-    var randomLetter= alphabet[Math.floor(Math.random() * alphabet.length)];
-    letterBoxes[i].innerHTML = randomLetter;
-  }
-  for (var i = 0; i < letterBoxes.length; i++) {
-    letterBoxes[i].addEventListener('click', onLetterClick)
-  }
-  document.querySelector('#clear').addEventListener('click', clearWord)
+	function reset() {
+		location.reload();
+	}
 
-  function selectletter() {
-    document.querySelectorAll(".letter")
-    // spelledWord.push(this.textContent());
-
-  }
-
-
-
-  function clearWord () {
-    // event.target.classList.add('selected-letter')
-    // document.getElementsByClassName("letter").style.backgroundColor = "rgb(235, 223, 199)";
-    // document.getElementsByClassName("letter").style.borderColor = "black";
-    // spelledStr = '';
-    // spelledWord = [];
-    var selectedLetters = document.querySelectorAll(".selected-letter");
-    for (var i = 0; i < selectedLetters.length; i++) {
-      selectedLetters[i].classList.remove("selected-letter")
-    }
-    document.getElementById("word").innerText = chosenLetters = '';
-    // document.getElementById("word").innerHTML = spelledWord = [];
-    // console.log('spelled Word = ', spelledWord);
-
-  }
-
-  function pointValue () {
-    var ptValue = spelledStr.length * 9;
-    return ptValue
-  }
+	function submit() {
+		var ptValue = chosenLetters.length * 9;
+		document.querySelector("#score").innerText = ptValue
+		document.querySelector(".wordList").innerText = chosenLetters
+		document.querySelector(".wordPoints").innerText = ptValue
+		clear()
+	}
 })()
